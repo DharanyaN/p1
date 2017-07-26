47de46ed49cc97320872be7eca0314b73eddc2be
DharanyaN <30457114+DharanyaN@users.noreply.github.com>
2017-07-26 11:02:47 +0530

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int b,l,h;
+clrscr();
+printf("\d Enter the numbers:");
+scanf("%d %d %d",&l,&b,&h);
+int total=2(l*b+l*h+b*h);
+int vol=(l*b*h);
+printf("%d",&total);
+printf("\n %d",&vol);
+}
